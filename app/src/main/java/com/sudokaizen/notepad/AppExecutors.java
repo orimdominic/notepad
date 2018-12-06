e19510ecf1c051f7ec332210ea4a177593104660
Orim Dominic <orimdominic@gmail.com>
2018-12-06 21:23:14 +0100

    chore: Modify AppExecutors

--- app/src/main/java/com/sudokaizen/notepad/AppExecutors.java
+++ app/src/main/java/com/sudokaizen/notepad/AppExecutors.java
@@ -1,40 +1,15 @@
 package com.sudokaizen.notepad;
 
-/*
- * Copyright (C) 2017 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package com.example.android.persistence.migrations;
 
 import android.os.Handler;
 import android.os.Looper;
 import android.support.annotation.NonNull;
-
-import androidx.annotation.NonNull;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
-/**
- * Global executor pools for the whole application.
- * <p>
- * Grouping tasks like this avoids the effects of task starvation (e.g. disk reads don't wait behind
- * webservice requests).
- */
 public class AppExecutors {
 
-    private static final int THREAD_COUNT = 3;
+    private static final int MAX_NUM_OF_THREADS = 3;
 
     private final Executor diskIO;
 
@@ -49,7 +24,7 @@ public class AppExecutors {
     }
 
     public AppExecutors() {
-        this(Executors.newSingleThreadExecutor(), Executors.newFixedThreadPool(THREAD_COUNT),
+        this(Executors.newSingleThreadExecutor(), Executors.newFixedThreadPool(MAX_NUM_OF_THREADS),
                 new MainThreadExecutor());
     }
 
