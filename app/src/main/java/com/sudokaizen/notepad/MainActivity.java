f2348597b1bc79f440236d795de36b4733c5d36b
Orim Dominic <orimdominic@gmail.com>
2018-12-06 23:59:33 +0100

    feat: Validate saving of notes through snackbar

--- app/src/main/java/com/sudokaizen/notepad/MainActivity.java
+++ app/src/main/java/com/sudokaizen/notepad/MainActivity.java
@@ -6,6 +6,7 @@ import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,12 +21,14 @@ public class MainActivity extends AppCompatActivity {
 
     private MainActivityViewModel mViewModel;
     private AppRepository mAppRepository;
+    private View rootLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        startActivity(new Intent(MainActivity.this, CreateNoteActivity.class));
+        rootLayout = findViewById(R.id.layout_activity_main);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -40,14 +43,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setupViewModel() {
+
         mViewModel = ViewModelProviders.of(MainActivity.this)
                 .get(MainActivityViewModel.class);
         mViewModel.getNotes().observe(MainActivity.this, new Observer<List<NoteEntry>>() {
             @Override
             public void onChanged(@Nullable List<NoteEntry> noteEntries) {
-                for (NoteEntry note: noteEntries) {
-                    System.out.println("Note " + note.getId() + " says "+ note.getContent());
+                StringBuilder allNotes = new StringBuilder();
+                for (NoteEntry note : noteEntries) {
+                    allNotes.append(note.getContent())
+                            .append("\n");
                 }
+                String notesString = allNotes.toString();
+                Snackbar mySnackbar = Snackbar.make(rootLayout, notesString, 3000);
+                mySnackbar.show();
             }
         });
     }
