5a0f07dc70439937e41564c5af228b3a70b97663
Orim Dominic <orimdominic@gmail.com>
2018-12-22 10:47:30 +0100

    style:
    Toolbar set to scrolling
    Note background stroke width reduced

--- app/src/main/java/com/sudokaizen/notepad/ui/NotesListActivity.java
+++ app/src/main/java/com/sudokaizen/notepad/ui/NotesListActivity.java
@@ -8,12 +8,14 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -39,9 +41,7 @@ import java.util.List;
 
 public class NotesListActivity extends AppCompatActivity {
 
-    private MainViewModel mViewModel;
     private RecyclerView rvNotes;
-    LinearLayoutManager rvLayoutManager;
     private NotesAdapter mNotesAdapter;
     private NoteRepository mNoteRepository;
     private UserRepository mUserRepository;
@@ -64,14 +64,16 @@ public class NotesListActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_notes_list);
 
+        Toolbar toolbar = findViewById(R.id.toolbar_notes_list);
+        setSupportActionBar(toolbar);
+
         mAppExecutors = new AppExecutors();
         mNoteRepository = NoteRepository.getInstance(NotesListActivity.this);
         mUserRepository = UserRepository.getInstance(this);
         FloatingActionButton fab = findViewById(R.id.fab);
         rvNotes = findViewById(R.id.rv_main_notes);
-        rvLayoutManager =
-                new LinearLayoutManager(NotesListActivity.this,
-                        LinearLayoutManager.VERTICAL, false);
+        LinearLayoutManager rvLayoutManager = new LinearLayoutManager(NotesListActivity.this,
+                LinearLayoutManager.VERTICAL, false);
         rvNotes.setLayoutManager(rvLayoutManager);
 
         new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
@@ -101,9 +103,9 @@ public class NotesListActivity extends AppCompatActivity {
 
     private void setupViewModel() {
 
-        mViewModel = ViewModelProviders.of(NotesListActivity.this)
+        MainViewModel viewModel = ViewModelProviders.of(NotesListActivity.this)
                 .get(MainViewModel.class);
-        mViewModel.getNotes().observe(NotesListActivity.this, new Observer<List<NoteEntry>>() {
+        viewModel.getNotes().observe(NotesListActivity.this, new Observer<List<NoteEntry>>() {
             @Override
             public void onChanged(@Nullable List<NoteEntry> noteEntries) {
                 if (noteEntries.size() == 0) {
